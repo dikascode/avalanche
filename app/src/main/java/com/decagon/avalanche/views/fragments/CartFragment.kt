cb9e1c64958d6a4cbcdee94909fdfd53d7cf30a8
dikascode <dikaemanuel@gmail.com>
2020-12-15 11:58:57 +0100

    Reset cart after checkout

--- app/src/main/java/com/decagon/avalanche/views/fragments/CartFragment.kt
+++ app/src/main/java/com/decagon/avalanche/views/fragments/CartFragment.kt
@@ -131,18 +131,21 @@ class CartFragment : Fragment(), CartListAdapter.CartInterface {
                     Log.d("Successful Transaction", "Transaction payment type: ${transactionResponse.get("paymentType")}")
                     Log.d("Successful Transaction", "Transaction msg: ${transactionResponse.get("vbvrespmessage")}")
 
-
                     findNavController().navigate(R.id.orderFragment)
                 }
                 RavePayActivity.RESULT_ERROR -> {
                     Toast.makeText(requireContext(), "ERROR", Toast.LENGTH_LONG).show()
                     Log.d("Failed Transaction", "Transaction status: ${transactionResponse.get("status")}")
                     Log.d("Failed Transaction", "Transaction msg: ${transactionResponse.get("vbvrespmessage")}")
+
+                    findNavController().navigate(R.id.mainFragment)
                 }
                 RavePayActivity.RESULT_CANCELLED -> {
                     Toast.makeText(requireContext(), "CANCELLED", Toast.LENGTH_LONG).show()
                     Log.d("Failed Transaction", "Transaction status: ${transactionResponse.get("status")}")
                     Log.d("Failed Transaction", "Transaction msg: ${transactionResponse.get("vbvrespmessage")}")
+
+                    findNavController().navigate(R.id.mainFragment)
                 }
             }
         } else {
--- app/src/main/java/com/decagon/avalanche/views/fragments/OrderFragment.kt
+++ app/src/main/java/com/decagon/avalanche/views/fragments/OrderFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.decagon.avalanche.R
 import com.decagon.avalanche.databinding.FragmentOrderBinding
+import com.decagon.avalanche.viewmodels.StoreViewModel
 
 class OrderFragment : Fragment() {
     private var _binding: FragmentOrderBinding? = null
     val binding get() = _binding!!
+
+    lateinit var storeViewModel: StoreViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +25,10 @@ class OrderFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = binding.root
 
+        val storeViewModel = ViewModelProvider(this).get(StoreViewModel::class.java)
+
         binding.continueShoppingBtn.setOnClickListener {
+            storeViewModel.resetCart()
             findNavController().navigate(R.id.mainFragment)
         }
 

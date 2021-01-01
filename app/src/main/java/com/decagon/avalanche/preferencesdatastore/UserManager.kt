fb71a819b493df8ae191526f2225dbb16dbdee7e
dikascode <dikaemanuel@gmail.com>
2021-01-02 00:43:51 +0100

    Error catch for datastore

--- app/src/main/java/com/decagon/avalanche/preferencesdatastore/UserManager.kt
+++ app/src/main/java/com/decagon/avalanche/preferencesdatastore/UserManager.kt
@@ -2,13 +2,16 @@ package com.decagon.avalanche.preferencesdatastore
 
 import android.content.Context
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.preferencesKey
 import androidx.datastore.preferences.createDataStore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import java.io.IOException
 
 class UserManager(context: Context) {
-    private  val dataStore = context.createDataStore("user_prefs")
+    private val dataStore = context.createDataStore("user_prefs")
 
     companion object {
         val USER_NAME_KEY = preferencesKey<String>("USER_FIRST_NAME")
@@ -24,15 +27,33 @@ class UserManager(context: Context) {
         }
     }
 
-    val userNameFlow: Flow<String> = dataStore.data.map {
-        it[USER_NAME_KEY] ?: ""
-    }
+    val userNameFlow: Flow<String> = dataStore.data.catch { exception ->
+        if (exception is IOException) {
+            emit(emptyPreferences())
+        } else {
+            throw exception
+        }
+    }.map {
+            it[USER_NAME_KEY] ?: ""
+        }
 
-    val userEmailFlow: Flow<String> = dataStore.data.map {
+    val userEmailFlow: Flow<String> = dataStore.data.catch { exception ->
+        if (exception is java.io.IOException) {
+            emit(emptyPreferences())
+        } else {
+            throw exception
+        }
+    }.map {
         it[USER_EMAIL_KEY] ?: ""
     }
 
-    val userPhoneFlow: Flow<String> = dataStore.data.map {
+    val userPhoneFlow: Flow<String> = dataStore.data.catch { exception ->
+        if (exception is IOException) {
+            emit(emptyPreferences())
+        } else {
+            throw exception
+        }
+    }.map {
         it[USER_PHONE_KEY] ?: ""
     }
 }

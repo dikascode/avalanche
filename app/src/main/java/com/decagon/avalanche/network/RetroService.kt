3d42a6d6875246e681ee0c2cdb2a898cd3637cf8
dikascode <dikaemanuel@gmail.com>
2021-01-27 10:05:10 +0100

    Updated retroservice with push notification Post request

--- app/src/main/java/com/decagon/avalanche/network/RetroService.kt
+++ app/src/main/java/com/decagon/avalanche/network/RetroService.kt
@@ -1,12 +1,25 @@
 package com.decagon.avalanche.network
 
-import androidx.lifecycle.LiveData
+
+import com.decagon.avalanche.constants.Constants.Companion.CONTENT_TYPE
+import com.decagon.avalanche.constants.Constants.Companion.SERVER_KEY
 import com.decagon.avalanche.data.Product
+import com.decagon.avalanche.data.PushNotification
+import okhttp3.ResponseBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
 interface RetroService {
 //   " data/products.json"
     @GET("api/ecommerce/v1/allProducts")
-    suspend fun getAllProducts(): Response<List<Product>>z
+    suspend fun getAllProducts(): Response<List<Product>>
+
+    @Headers("Authorization: key=${SERVER_KEY}", "Content-Type:${CONTENT_TYPE}")
+    @POST("fcm/send")
+    suspend fun postNotification(
+        @Body notification: PushNotification
+    ):Response<ResponseBody>
 }

5d0af8e5f89944a51dbb8585defdb8311f15fe09
dikascode <dikaemanuel@gmail.com>
2021-02-24 04:09:23 +0100

    Changed mail for javamail

--- app/src/main/java/com/dikascode/avalanche/constants/Constants.kt
+++ app/src/main/java/com/dikascode/avalanche/constants/Constants.kt
@@ -9,8 +9,8 @@ class Constants {
         const val SERVER_KEY = "AAAAqBjX8wY:APA91bEg-F7_yJnfCJCZJCy2EPC0BrC6zEZ9SEGtI5G604eSmEbJuQmhez4jwMfYMQZuuKnZNH8kAp7S7LvLYpWJMIgDNXI5_hhjUvENkxr3UannI7ZTrJoeFSfQ6IayWYHHaqP0t63W"
         const val CONTENT_TYPE = "application/json"
 
-        const val EMAIL = "lexypoet@gmail.com"
-        const val PASSWORD = "bestiemi"
+        const val EMAIL = "avalanchesignature@gmail.com"
+        const val PASSWORD = "\$avalanche123"
 
         /** Formatting prices */
         val FORMATTER: DecimalFormat? = DecimalFormat("#,###,###")
